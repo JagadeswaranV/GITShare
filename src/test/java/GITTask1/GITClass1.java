d966618d6a3e9bc8ca5f61a89ce42d5ba81c9870
JagadeswaranV <jagadesv27@gmail.com>
2022-12-04 21:07:57 +0530

    MasterByJagades

--- src/test/java/GITTask1/GITClass1.java
+++ src/test/java/GITTask1/GITClass1.java
@@ -9,4 +9,19 @@ public void m1()
 {
 	System.out.println("m1 by Jagades");
 }
+@Test
+public void m5()
+{
+	System.out.println("m5 by Jagades");
+}
+@Test
+public void m6()
+{
+	System.out.println("m6 by Jagades");
+}
+@Test
+public void m7()
+{
+	System.out.println("m7 by Jagades");
+}
 }
